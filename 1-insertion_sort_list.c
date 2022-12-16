f469211714e194361160731ba9d32ca0047f1cb4
CaroChoch <fleur-a-fleur59@hotmail.fr>
2022-12-16 17:15:20 +0100

    function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort that sorts an array of integers in ascending
+ * order
+ * @array: array
+ * @size: size of array
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, tmp, min = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		if (min != i)
+			print_array(array, size);
+	}
+}
+
